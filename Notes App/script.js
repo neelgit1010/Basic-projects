d7c1b624952431aab1f7da6d7aa064412a28d3a9
neelgit1010 <neel.binbrain1010@gmail.com>
2024-02-26 14:31:24 +0530

    Save btn added

--- Notes App/script.js
+++ Notes App/script.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('.main');
-let notes = document.querySelector('.input');
+let notes = document.querySelectorAll('.input');
+
 
 function createNote() {
     let input = document.createElement('p'); // creating a p tag
@@ -8,15 +9,35 @@ function createNote() {
     let imgDel = document.createElement('img');
     imgDel.src = 'delete.png';
     main.appendChild(input).appendChild(imgDel);
+    storeData();
 }
+function showNotes() {
+    main.innerHTML = localStorage.getItem('note');
+    console.log('showing');
+}
+showNotes();
 
 function storeData() {
-    localStorage.setItem('notes',main.innerHTML);
+    localStorage.setItem('note',main.innerHTML);
+    console.log('storing');
 }
 
 main.addEventListener('click', (e) =>{
     if(e.target.tagName === 'IMG'){
-        storeData();
         e.target.parentElement.remove();
+        storeData();
+    }else if(e.target.tagName === 'p'){
+        notes = document.querySelectorAll('.input');
+        notes.forEach(n =>{
+            n.onkeyup = function(){
+                storeData();
+            }
+        });
     }
-})
+});
+
+document.getElementById('save').addEventListener('click', e=>{
+    storeData();
+});
+
+
